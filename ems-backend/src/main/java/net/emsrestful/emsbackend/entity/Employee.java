f2f379b310502a85f9eb4e5ef9c507b31dcdae5b
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-02-10 23:43:53 +0545

    field_annoted-entity

---
+++ ems-backend/src/main/java/net/emsrestful/emsbackend/entity/Employee.java
@@ -0,0 +1,33 @@
+package net.emsrestful.emsbackend.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity //basic to speciy class as jpa entity
+@Table(name = "employees")  //basic to specity table data
+
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    private String address;
+    @Column(name = "phone_number")
+    private Long phone;
+}
