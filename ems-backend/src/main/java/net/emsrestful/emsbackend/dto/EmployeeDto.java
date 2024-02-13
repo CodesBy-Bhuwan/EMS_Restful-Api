9076309a8e7499c783e2b1931551569f9a8e11c9
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-02-13 22:57:29 +0545

    Annotation&&classEmployeeDto

--- ems-backend/src/main/java/net/emsrestful/emsbackend/dto/EmployeeDto.java
+++ ems-backend/src/main/java/net/emsrestful/emsbackend/dto/EmployeeDto.java
@@ -1,2 +1,32 @@
-package net.emsrestful.emsbackend.dto;public class Employeedto {
+package net.emsrestful.emsbackend.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+// This dto is to transfer the data between client and server
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+//this employeeDto will be the response for https
+public class EmployeeDto {
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private Long phone;
+
+    private String address;
+
+
+
 }
