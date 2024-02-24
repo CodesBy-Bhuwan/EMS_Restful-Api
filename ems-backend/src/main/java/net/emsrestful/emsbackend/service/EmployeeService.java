6434f95557d88e76d62a85d862565c4369c5ef5d
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-02-24 23:38:25 +0545

    List-Dto_ById

--- ems-backend/src/main/java/net/emsrestful/emsbackend/service/EmployeeService.java
+++ ems-backend/src/main/java/net/emsrestful/emsbackend/service/EmployeeService.java
@@ -2,8 +2,11 @@ package net.emsrestful.emsbackend.service;
 
 import net.emsrestful.emsbackend.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+    List<EmployeeDto> getAllEmployees();
 }
